907a7909f3ec01b413176cf52ccc7f8c6044adff
Van Hoang <hoangray2309@gmail.com>
2024-09-06 08:19:21 +0700

    forloop

--- hocjavametqua.java
+++ hocjavametqua.java
@@ -1,16 +1,9 @@
 package JAVA;
 public class hocjavametqua {
     public static void main(String[] args) {
-        int dice=1;
-        while (dice<=6) {
-            if (dice<6) {
-                System.out.println("thua me r");
-            }
-            else{
-                System.out.println("yeee thang r");
-            }
-            dice= dice +1;    
-        }
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+          }
   }
 }
 
