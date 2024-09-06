d8e70e34883603490206f25f7e4bcb360473b362
Van Hoang <hoangray2309@gmail.com>
2024-09-06 19:07:52 +0700

    baimoi

---
+++ code.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int thang;
+//     cin>>thang;
+//     switch (thang)
+//     {
+//      case 2:
+//      case 3:
+//      case 4:
+//      cout << "Mua Xuan";
+//      case 5:
+//      case 6:
+//      case 7:
+//      cout << "Mua He";
+//      case 8:
+//      case 9:
+//      case 10:
+//      cout << "Mua Thu";
+//      case 11:
+//      case 12:
+//      case 1:
+//      cout << "Mua Dong";
+//      break;
+
+//  }
+// }
+
+
+
+
