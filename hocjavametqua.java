8f5310b4ca6668488effde7dfd4e7e2bfd1b3c09
Van Hoang <hoangray2309@gmail.com>
2024-09-06 08:15:32 +0700

    javane

---
+++ hocjavametqua.java
@@ -0,0 +1,17 @@
+package JAVA;
+public class hocjavametqua {
+    public static void main(String[] args) {
+        int dice=1;
+        while (dice<=6) {
+            if (dice<6) {
+                System.out.println("thua me r");
+            }
+            else{
+                System.out.println("yeee thang r");
+            }
+            dice= dice +1;    
+        }
+  }
+}
+
+
